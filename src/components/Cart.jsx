13b24663dbd2ea2ac08f5eb3929848ba9288807c
Charanjeet singh <152056551+charn13@users.noreply.github.com>
2024-04-09 13:44:21 +0530

    Update Cart.jsx

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -19,7 +19,7 @@ export default function Cart() {
   const handleCheckOut = async () => {
     let userEmail = localStorage.getItem("userEmail");
     // console.log(data,localStorage.getItem("userEmail"),new Date())
-    let response = await fetch("https://gofood-1-pmft.onrender.com/api/orderData", {
+    let response = await fetch("https://newfoodgo.onrender.com/api/orderData", {
       // credentials: 'include',
       // Origin:"http://localhost:3000/login",
       method: 'POST',
